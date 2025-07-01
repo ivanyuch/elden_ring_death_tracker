a3fe725c5485017a212e3f7a27cf54fa99943249
Ivan Yu <iyu963@aucklanduni.ac.nz>
2025-07-01 19:51:10 +1200

    Make code stay running, will now print commands which are to be implemented.

--- DeathCounter.java
+++ DeathCounter.java
@@ -3,28 +3,40 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class DeathCounter {
-    public static void main(String[] args) {
-      try (BufferedReader reader = new BufferedReader(new FileReader("deaths.txt"))) {
-        String line = reader.readLine();
-        String[] parts = line.split(",");
-        int margit = Integer.parseInt(parts[0]);
-        int godrick = Integer.parseInt(parts[1]);
-        int wolf = Integer.parseInt(parts[2]);
-        int rennala = Integer.parseInt(parts[3]);
-        int radahn = Integer.parseInt(parts[4]);
-        int rykard = Integer.parseInt(parts[5]);
-        int shade = Integer.parseInt(parts[6]);
-        int morgott = Integer.parseInt(parts[7]);
-        int giant = Integer.parseInt(parts[8]);
-        int duo = Integer.parseInt(parts[9]);
-        int maliketh = Integer.parseInt(parts[10]);
-        int gideon = Integer.parseInt(parts[11]);
-        int godfrey = Integer.parseInt(parts[12]);
-        int radagon = Integer.parseInt(parts[13]);
+  public static void main(String[] args) {
+    try (BufferedReader reader = new BufferedReader(new FileReader("deaths.txt"))) {
+      String line = reader.readLine();
+      String[] parts = line.split(",");
+      int margit = Integer.parseInt(parts[0]);
+      int godrick = Integer.parseInt(parts[1]);
+      int wolf = Integer.parseInt(parts[2]);
+      int rennala = Integer.parseInt(parts[3]);
+      int radahn = Integer.parseInt(parts[4]);
+      int rykard = Integer.parseInt(parts[5]);
+      int shade = Integer.parseInt(parts[6]);
+      int morgott = Integer.parseInt(parts[7]);
+      int giant = Integer.parseInt(parts[8]);
+      int duo = Integer.parseInt(parts[9]);
+      int maliketh = Integer.parseInt(parts[10]);
+      int gideon = Integer.parseInt(parts[11]);
+      int godfrey = Integer.parseInt(parts[12]);
+      int radagon = Integer.parseInt(parts[13]);
 
-      } catch (IOException e) {
-        System.err.println("Error reading the file: " + e.getMessage());
-      }
+    } catch (IOException e) {
+      System.err.println("Error reading the file: " + e.getMessage());
     }
+
+    while (true) {
+      printCommands();
+    }
+  }
+
+  public static void printCommands () {
+    System.out.println("\nCommands:");
+    System.out.println("\n'show': Show all deaths");
+    System.out.println("\n'select': select a boss to track");
+    System.out.println("\n'help': provides more information about the commands");
+    System.out.println("\n'exit': exit the program");
+  }
   
 }
